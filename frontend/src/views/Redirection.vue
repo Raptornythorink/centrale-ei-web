f3e8e095b02230114bbe429556a4c2cc0d4ee1f5
Aymeric Chaumont <aymeric.chaumont@student-cs.fr>
2022-06-10 14:01:55 +0200

    redirection page & fix missing movie descs

---
+++ frontend/src/views/Redirection.vue
@@ -0,0 +1,13 @@
+<template><div></div></template>
+
+<script>
+export default {
+  name: "Redirection",
+
+  created: function () {
+    this.$router.push("/home/");
+  },
+};
+</script>
+
+<style scoped></style>
